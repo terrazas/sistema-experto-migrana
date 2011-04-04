46357b973c3c0a86061ef6c81a66b5ed0000773e
silviamary17 <silviamary17@ac87c8f5-84e2-856a-78e6-8afca1af3942>
2011-04-04 17:16:26 +0000

    Primer intento

---
+++ trunk/Experto/src/BaseConocimientos.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author silvia
+ */
+public class BaseConocimientos {
+    private String Migraña_con_Aura;
+}
